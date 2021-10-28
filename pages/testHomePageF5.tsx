d1f1ba726110b108801bd86037f23b937ece0e5d
TrungDuc99 <dotrungduc.bl@gmail.com>
2021-10-28 20:52:34 +0700

    chỉnh sửa giao diện

--- pages/testHomePageF5.tsx
+++ pages/testHomePageF5.tsx
@@ -2,9 +2,11 @@ import BannerF5seconds from '@component/banner/BannerF5seconds'
 import Footer_f5seconds from '@component/footer/Footer_f5seconds'
 import AboutUsTimeLine from '@component/home-f5seconds/AboutUsTimeLine'
 import F5secondsCare from '@component/home-f5seconds/F5secondsCare'
+import InfoDetail from '@component/home-f5seconds/InfoDetail'
+import ListOurPeople from '@component/home-f5seconds/ListOurPeople'
 import ListPortfolio from '@component/home-f5seconds/ListPortfolio'
 import AppLayout_test from '@component/layout/AppLayout_test'
-
+import styles from '../src/styles/Home.module.css'
 const IndexPage = () => {
   return (
     <AppLayout_test>
@@ -12,6 +14,9 @@ const IndexPage = () => {
       <ListPortfolio />
       <F5secondsCare />
       <AboutUsTimeLine />
+      <ListOurPeople />
+      <div className={styles.imgBgFooter}></div>
+      <InfoDetail />
       <Footer_f5seconds />
     </AppLayout_test>
   )
--- src/components/banner/BannerF5seconds.tsx
+++ src/components/banner/BannerF5seconds.tsx
@@ -1,7 +1,7 @@
-import BazarImage from '@component/BazarImage'
+// import BazarImage from '@component/BazarImage'
 import { Box } from '@material-ui/core'
 import React, { FC } from 'react'
-
+import styles from '../../../src/styles/Home.module.css'
 // styled component
 
 const BannerF5seconds: FC = () => {
@@ -18,15 +18,17 @@ const BannerF5seconds: FC = () => {
           flexDirection: 'row',
         }}
       >
-        <BazarImage
+        <div className={styles.imgBanner}></div>
+        {/* <BazarImage
           src="assets/images/f5seconds/banner-header.jpg"
           alt="hinh-thuong-hieu"
           style={{
             width: '100%',
           }}
-        ></BazarImage>
+        ></BazarImage> */}
 
         <h1
+          className={styles.titleF5seconds}
           style={{
             fontSize: '6.5rem',
             lineHeight: '6.5rem',
@@ -35,7 +37,6 @@ const BannerF5seconds: FC = () => {
             padding: '10px',
             color: 'rgba(255, 255, 255, 0.7)',
             position: 'absolute',
-
             left: '50%',
             transform: 'translate(-50%, -50%)',
           }}
@@ -43,6 +44,7 @@ const BannerF5seconds: FC = () => {
           F5SECONDS
         </h1>
         <h2
+          className={styles.undertitleF5seconds}
           style={{
             fontSize: '1.25rem',
             lineHeight: '6.5rem',
